a4f5e7c1d860bdcda300a587a886f3653bb7933e
Pawel Grzesik <pawel@Pawels-MBP.fritz.box>
2018-03-05 08:27:07 +0000

    Updated really old code.

--- mailProxy.go
+++ mailProxy.go
@@ -1,5 +1,7 @@
 /**
 #
+# Pawel Grzesik, 2016
+#
 # To run as a test:
 # > go run script
 #
@@ -9,10 +11,20 @@
 #
 **/
 
+/*
+  Main Package
+*/
 package main
 
+/*
+  Here we are importing few modules that
+  we need to use.
+*/
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"math/rand"
@@ -25,55 +37,138 @@ import (
 	"time"
 )
 
-var queue string
+/*
+  ARRAY
+  OURDOMAIN: domain list that we want to check
+*/
+var OURDOMAIN = [2]string{"foobar.org",
+	"foobar.com"}
 
-const OURDOMAIN string = "mydomain"
-const PROXYDIR string = "/tmp/proxy"
+/*
+  SLICE
+  WHITELIST: we can whitelist e-mails by name
+*/
+var WHITELIST = [...]string{"alerter.test",
+	"system.email",
+	"jira",
+	"alerter.live",
+	"salesforce.com",
+	"sf.com"}
+
+var CheckUserList = [...]string{"pawel.grzesik"}
+
+/*
+  PROXYDIR: main directory for the proxyMail tool
+  PROXYARCHIVE: directory for the queue (blocked mails)
+  PROXYLOG: directory/path for the log file
+*/
+const PROXYDIR string = "/var/spool/mailProxy/"
 const PROXYARCHIVE string = "/queue"
 const PROXYLOG string = "/logs/proxy.log"
 
+/*
+  E-mail Notification Settings
+*/
+const NotificationFrom string = "mailProxy@"
+const NotificationRecipients string = "pawel.grzesik@"
+const NotificationSubject string = "mailProxy problem!"
+
+/*
+  Our main MailStruct struct type
+  MailQueue: queue_num
+  BackupFile: path to the blocked mail
+  LogFile: log file
+  MailData: raw mail source
+*/
+type MailStruct struct {
+	MailQueue  string
+	BackupFile string
+	LogFile    string
+	MailData   []byte
+}
+
+type APIStruct struct {
+	Sender       string `json:"sender"`
+	SenderHeader string `json:"senderheader"`
+	Recipient    string `json:"recipient"`
+	Queue        string `json:"queue`
+	Blocked      bool   `json:"blocked"`
+}
+
+/*
+  DEBUG MODE
+  true or false
+*/
+const DEBUG bool = false
+
 func main() {
 	/*
-	  Needed for random queue string
+	  Needed for random queue string every time when we execute the code
 	*/
 	rand.Seed(time.Now().UnixNano())
 
 	/*
 	  Generate random string with 10 characters.
 	*/
-	queue = RandStringBytesRmndr(10)
+	queue, err := RandStringBytesRmndr(10)
+	if err != nil {
+		log.Println("Problem with RandStringBytesRmndr: ", err)
+		os.Exit(0)
+	}
 
 	/*
 	  Setup correct PATHs for the app
-	  PROXYDIR is where the app is going to work
-	  PROXYARCHIVE is where we are keeping blocked e-mails
-	  PROXYLOG is location for our log file
 	*/
 	archiveFile := path.Join(PROXYDIR, PROXYARCHIVE, queue)
 	logFile := path.Join(PROXYDIR, PROXYLOG)
+
+	/*
+	   Initialization
+	   This allocates memory for all the fields, sets each of them
+	   to their zero value and returns a pointer.
+	*/
+	s := &MailStruct{
+		MailQueue:  queue,
+		BackupFile: archiveFile,
+		LogFile:    logFile,
+	}
 
 	/*
 	  Open and deal with the log file
 	*/
 	f, err := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
 	if err != nil {
-		log.Fatalf("error opening file: %v", err)
+		log.Println(s.MailQueue+" Problem with opening os.OpenFile(): ", err)
+		s.saveMail()
+		s.sendNotification("Problem with opening os.OpenFile()")
+		os.Exit(0)
 	}
 	defer f.Close()
 
 	log.SetOutput(f)
 
 	/*
-	   Call stats API
+	   Read mail source (raw) from the STDIN
 	*/
-	statsAPI("hit")
+	struct_err := s.readData()
+	if struct_err != nil {
+		log.Println(s.MailQueue+" Problem with maildata: ", struct_err)
+		s.sendNotification("Problem with maildata")
+	}
+
+	/*
+	   From now on we need to backup mail raw source in case of any problems.
+	*/
 
 	/*
 	  Check who is the sender from the mail Headers
 	*/
 	sender, err := MailSender()
+	log.Println(s.MailQueue + " Sender from args postfix: " + sender)
 	if err != nil {
-		log.Println(queue+" no sender ", err)
+		log.Println(s.MailQueue+" no sender ", err)
+		s.saveMail()
+		s.sendNotification("no sender")
 		os.Exit(0)
 	}
 
@@ -81,18 +176,11 @@ func main() {
 	  Check recipients and create string from them
 	*/
 	recipients, err := MailRecipients()
-	log.Println(recipients)
+	log.Println(s.MailQueue + " Recipients from args postfix: " + recipients)
 	if err != nil {
-		log.Println(queue+" no recipients ", err)
-		os.Exit(0)
-	}
-
-	/*
-	  Read mail source (raw) from the STDIN
-	*/
-	maildata, err := ioutil.ReadAll(os.Stdin)
-	if err != nil {
-		log.Fatal(err)
+		log.Println(s.MailQueue+" no recipients ", err)
+		s.saveMail()
+		s.sendNotification("no recipients")
 		os.Exit(0)
 	}
 
@@ -101,54 +189,211 @@ func main() {
 	  If we cannot verify it, e-mail will be send
 	  and save for the later investigation
 	*/
-	senderHeader, err := MailSenderHeader(maildata)
+
+	// Convert raw mail to string
+	mailString, err := stdinToString(s.MailData)
 	if err != nil {
-		log.Println(queue+"senderHeader() err: ", err)
-		doMail := sendMail(sender, recipients, maildata)
-		if doMail != nil {
-			log.Println(queue+" sendMail() ", doMail)
-		}
-		aMail := saveMail(archiveFile, maildata)
-		if aMail != nil {
-			log.Println(queue+" sendMail() error: ", aMail)
-		}
+		log.Println(s.MailQueue+" Cannot convert raw mail to string ", err)
+		s.saveMail()
+		s.sendNotification("Cannot convert raw mail to string")
+		os.Exit(0)
+	}
+
+	// Read mail and return *mail.Message
+	mM, err := readMail(mailString)
+	if err != nil {
+		log.Println(s.MailQueue+" Cannot convert mail to *mail.Message ", err)
+		s.saveMail()
+		s.sendNotification("Cannot convert mail to *mail.Message")
+		sendMail(sender, recipients, s.MailData)
+		os.Exit(0)
+	}
+
+	// Check header
+	header, err := mailHeader(mM)
+	if err != nil {
+		log.Println(s.MailQueue+" Cannot parse mailHeader ", err)
+		s.saveMail()
+		s.sendNotification("Cannot parse mailHeader")
+		sendMail(sender, recipients, s.MailData)
+		os.Exit(0)
+	}
+
+	// Check "From" from header
+	senderHeader, err := MailSenderHeader(header)
+	log.Println(s.MailQueue + " Sender from *mail.Message: " + senderHeader)
+	if err != nil {
+		log.Println(s.MailQueue+" Cannot check From header: ", err)
+		s.saveMail()
+		s.sendNotification("Cannot check From header")
+		sendMail(sender, recipients, s.MailData)
+		os.Exit(0)
+	}
+
+	// Check if Sender From has a correct format
+	senderFromFormat, err := senderFormat(senderHeader)
+	if err != nil {
+		log.Println(s.MailQueue+" Cannot check From format: ", err)
+		s.saveMail()
+		s.sendNotification("Cannot check From format")
+		sendMail(sender, recipients, s.MailData)
+		os.Exit(0)
+	}
+
+	if senderFromFormat == false {
+		log.Println(s.MailQueue+" Wrong From format: ", err)
+		s.saveMail()
+		s.sendNotification("Wrong From format")
+		sendMail(sender, recipients, s.MailData)
+		os.Exit(0)
 	}
 
 	/*
-	  Checking if sender is from OURDOMAIN.
-	  Block if it is.
-	  Pass if it's not.
+	  Checking if sender is in our WHITELIST
+	  Pass an e-mail if it is.
 	*/
-	if CheckDomain(senderHeader) == true {
-		statsAPI("block")
-		log.Println(queue + " BLOCKED: " + sender + " => " + recipients)
-		n_from := "proxy@mydomain"
-		n_to := "alert@"
-		n_body := []byte(queue + " BLOCKED: " + sender + " => " + recipients)
-
-		// Send Mail
-		sMail := sendMail(n_from, n_to, n_body)
-		if sMail != nil {
-			log.Println(queue+" sendMail() error: ", sMail)
-		}
-
-		// Archive Mail
-		log.Println(queue + " saved to: " + archiveFile)
-		aMail := saveMail(archiveFile, maildata)
-		if aMail != nil {
-			log.Println(queue+" sendMail() error: ", aMail)
-		}
-
-		// Exit with 0
+	// return user@domain.com (from ARG)
+	userFromArg, err := CheckUserNameFromList(sender)
+	if err != nil {
+		log.Println(s.MailQueue+" Error when checking userFromArg: ", err)
+		s.saveMail()
+		s.sendNotification("Error when checking userFromArg")
+		sendMail(sender, recipients, s.MailData)
 		os.Exit(0)
-	} else {
-		statsAPI("pass")
-		log.Println(queue + " PASSED: " + sender + " => " + recipients)
-		doMail := sendMail(sender, recipients, maildata)
-		if doMail != nil {
-			log.Println(queue+" sendMail() ", doMail)
-		}
 	}
+
+	// return user@domain.com (from Headers)
+	userFromHeaders, err := CheckUserNameFromList(senderHeader)
+	if err != nil {
+		log.Println(s.MailQueue+" Error when checking userFromHeaders: ", err)
+		s.saveMail()
+		s.sendNotification("Error when checking userFromHeaders")
+		sendMail(sender, recipients, s.MailData)
+		os.Exit(0)
+	}
+
+	// checking if user is on the list
+	userResult, err := CheckUserFromList(sender)
+	if err != nil {
+		log.Println(s.MailQueue+" Error when checking userResult: ", err)
+		s.saveMail()
+		s.sendNotification("Error when checking userResult")
+		sendMail(sender, recipients, s.MailData)
+		os.Exit(0)
+	}
+
+	// checking if user is on the list
+	userResultHeader, err := CheckUserFromList(senderHeader)
+	if err != nil {
+		log.Println(s.MailQueue+" Error when checking userResultHeader: ", err)
+		s.saveMail()
+		s.sendNotification("Error when checking userResultHeader")
+		sendMail(sender, recipients, s.MailData)
+		os.Exit(0)
+	}
+
+	/*
+	   Checking if sender is from OURDOMAIN.
+	   Block if it is.
+	   Pass if it's not.
+	*/
+	domainResult, err := CheckDomain(senderHeader)
+	if err != nil {
+		log.Println(s.MailQueue+" Error when checking domainResult: ", err)
+		s.saveMail()
+		s.sendNotification("Error when checking domainResult")
+		sendMail(sender, recipients, s.MailData)
+		os.Exit(0)
+	}
+
+	// checking whitelist
+	whitelistDomainFromArg, err := whitelistDomainCheck(sender)
+	if err != nil {
+		log.Println(s.MailQueue+" Error when checking whitelistDomainFromArgt: ", err)
+		s.saveMail()
+		s.sendNotification("Error when checking whitelistDomainFromArg")
+		sendMail(sender, recipients, s.MailData)
+		os.Exit(0)
+	}
+
+	// checking whitelist
+	whitelistDomainFromHeaders, err := whitelistDomainCheck(senderHeader)
+	if err != nil {
+		log.Println(s.MailQueue+" Error when checking whitelistDomainFromHeaders: ", err)
+		s.saveMail()
+		s.sendNotification("Error when checking whitelistDomainFromHeaderst")
+		sendMail(sender, recipients, s.MailData)
+		os.Exit(0)
+	}
+
+	// check whitelist domains
+	if whitelistDomainFromArg == true || whitelistDomainFromHeaders == true {
+		log.Println(s.MailQueue + " PASSED (WHITELISTED domain): " + sender + " => " + recipients)
+		doMail := sendMail(sender, recipients, s.MailData)
+		if doMail != nil {
+			log.Println(s.MailQueue+" sendMail() ", doMail)
+		}
+		os.Exit(0)
+	}
+
+	// if domainResult == true {
+	// and userResult || userResultHeader (those are addresses from the SLICE LIST)
+	if domainResult == true && ((userResult == true) || (userResultHeader == true)) {
+		if userFromArg != userFromHeaders {
+			// Log it
+			log.Println(s.MailQueue + " BLOCKED: " + sender + " => " + recipients)
+
+			// Send notification
+			s.sendNotification("BLOCKED: " + sender + "=>" + recipients)
+
+			// Archive Mail
+			log.Println(s.MailQueue + " saved to: " + archiveFile)
+			s.saveMail()
+
+			call := &APIStruct{
+				Sender:       sender,
+				SenderHeader: senderHeader,
+				Recipient:    recipients,
+				Queue:        queue,
+				Blocked:      true,
+			}
+
+			// Check DEBUG mode
+			if DEBUG == true {
+				doMail := sendMail(sender, recipients, s.MailData)
+				if doMail != nil {
+					log.Println(s.MailQueue+" sendMail(debug=true) ", doMail)
+				}
+				log.Println(s.MailQueue + " mail has been sent (DEBUG=true) ")
+				call.Blocked = false
+				err = call.api()
+				if err != nil {
+					os.Exit(0)
+				}
+				os.Exit(0)
+			} else {
+				err = call.api()
+				if err != nil {
+					os.Exit(0)
+				}
+				os.Exit(0)
+			}
+		}
+		// Log it
+		log.Println(s.MailQueue + " PASSED: " + sender + " => " + recipients)
+		doMail := sendMail(sender, recipients, s.MailData)
+		if doMail != nil {
+			log.Println(s.MailQueue+" sendMail() ", doMail)
+		}
+		os.Exit(0)
+	}
+	// Log it
+	log.Println(s.MailQueue + " PASSED: " + sender + " => " + recipients)
+	doMail := sendMail(sender, recipients, s.MailData)
+	if doMail != nil {
+		log.Println(s.MailQueue+" sendMail() ", doMail)
+	}
+	os.Exit(0)
 }
 
 /*
@@ -174,9 +419,7 @@ func MailRecipients() (string, error) {
 /*
   return MAIL_FROM (checking mail body)
 */
-func MailSenderHeader(maildata []byte) (string, error) {
-	mailString := stdinToString(maildata)
-	header := mailHeader(readMail(mailString))
+func MailSenderHeader(header mail.Header) (string, error) {
 	sender := header.Get("From")
 	return sender, nil
 }
@@ -184,30 +427,90 @@ func MailSenderHeader(maildata []byte) (string, error) {
 /*
   Generate random string for a mail queue
 */
-func RandStringBytesRmndr(n int) string {
+func RandStringBytesRmndr(n int) (string, error) {
 	const letterBytes = "123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	b := make([]byte, n)
 	for i := range b {
 		b[i] = letterBytes[rand.Int63()%int64(len(letterBytes))]
 	}
-	return string(b)
+	return string(b), nil
+}
+
+/*
+  Checking if sender has a corrent format
+*/
+func senderFormat(sender string) (bool, error) {
+	senderOk := true
+	numAt := strings.Count(sender, "@")
+	if (numAt != 1) && (numAt != 2) {
+		senderOk = false
+	}
+	return senderOk, nil
 }
 
 /*
   Checking if OURDOMAIN is the same as MAIL_FROM
 */
-func CheckDomain(sender string) bool {
-	var rbool bool
+func CheckDomain(sender string) (bool, error) {
+	rbool := false
 	host := strings.Split(sender, "@")[1]
-
-	if host == OURDOMAIN {
-		log.Println(queue + " checking " + host + " == " + OURDOMAIN)
-		rbool = true
-	} else {
-		log.Println(queue + " checking " + host + " != " + OURDOMAIN)
-		rbool = false
+	hostfinal := strings.Split(host, ">")[0]
+	for _, domain := range OURDOMAIN {
+		// log.Println("Checking domain: " + domain)
+		if hostfinal == domain {
+			// log.Println(domain + " == " + hostfinal)
+			rbool = true
+			break
+		}
 	}
-	return rbool
+	return rbool, nil
+}
+
+/*
+  Checking if e-mail name is in our WHITELIST slice
+*/
+func whitelistDomainCheck(sender string) (bool, error) {
+	rbool := false
+	domain := strings.Split(sender, "@")[1]
+	if len(strings.Split(domain, "<")) == 2 {
+		domain = strings.Split(domain, "<")[1]
+	}
+	for _, w := range WHITELIST {
+		if domain == w {
+			rbool = true
+			break
+		}
+	}
+	return rbool, nil
+}
+
+func CheckUserFromList(sender string) (bool, error) {
+	rbool := false
+	user := strings.Split(sender, "@")[0]
+	if len(strings.Split(user, "<")) == 2 {
+		user = strings.Split(user, "<")[1]
+	}
+	user = strings.ToLower(user)
+	for _, w := range CheckUserList {
+		// log.Println("Checking user from CheckUserList: " + user + "==" + w)
+		if user == w {
+			rbool = true
+			break
+		}
+	}
+	return rbool, nil
+}
+
+func CheckUserNameFromList(sender string) (string, error) {
+	user := strings.Split(sender, "@")[0]
+	domain := strings.Split(sender, "@")[1]
+
+	if len(strings.Split(user, "<")) == 2 {
+		user = strings.Split(user, "<")[1]
+		tmp := strings.Split(sender, "@")[1]
+		domain = strings.Split(tmp, ">")[0]
+	}
+	return user + "@" + domain, nil
 }
 
 /*
@@ -221,12 +524,12 @@ func sendMail(from, recipients string, maildata []byte) error {
 	sendmail := exec.Command("/usr/sbin/sendmail", "-G", "-i", "-f", from, "--", recipients)
 	pipe, err := sendmail.StdinPipe()
 	if err != nil {
-		log.Fatal(err)
+		log.Println("Problem with sendmail() method: ", err)
 	}
 
 	err = sendmail.Start()
 	if err != nil {
-		log.Fatal(err)
+		log.Println("Problem with sendmail() method: ", err)
 	}
 
 	fmt.Fprintf(pipe, "%s", maildata)
@@ -236,49 +539,83 @@ func sendMail(from, recipients string, maildata []byte) error {
 }
 
 /*
-  Save e-mail for later investigation
-*/
-func saveMail(archiveFile string, maildata []byte) error {
-	err := ioutil.WriteFile(archiveFile, maildata, 0644)
-	return err
-}
-
-/*
   Convert mail raw to string
 */
-func stdinToString(maildata []byte) *strings.Reader {
+func stdinToString(maildata []byte) (*strings.Reader, error) {
 	r := strings.NewReader(string(maildata))
-	return r
+	return r, nil
 }
 
 /*
   Return header from the Mail.
 */
-func mailHeader(m *mail.Message) mail.Header {
+func mailHeader(m *mail.Message) (mail.Header, error) {
 	header := m.Header
-	return header
+	return header, nil
 }
 
 /*
   Read mail string and return *mail.Message
 */
-func readMail(r *strings.Reader) *mail.Message {
+func readMail(r *strings.Reader) (*mail.Message, error) {
 	m, err := mail.ReadMessage(r)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return m
+	return m, err
 }
 
 /*
-  Stats API needed for Sensu/Monitoring
+  Read mail and add it to the MailStruct
 */
-func statsAPI(value string) {
-	url := "http://localhost:8000/" + value
-	resp, err := http.Get(url)
+func (s *MailStruct) readData() error {
+	data, err := ioutil.ReadAll(os.Stdin)
+	s.MailData = data
+	return err
+}
+
+/*
+  Save mail. Reasons:
+    - blocked
+	- error
+*/
+func (s *MailStruct) saveMail() error {
+	err := ioutil.WriteFile(s.BackupFile, s.MailData, 0644)
+	return err
+}
+
+/*
+  Send e-mail notification
+*/
+func (s *MailStruct) sendNotification(body string) error {
+
+	from := NotificationFrom
+	recipients := NotificationRecipients
+	subject := NotificationSubject
+	MailBody := s.MailQueue + " " + body
+
+	sendmail := exec.Command("/usr/sbin/sendmail", "-G", "-i", "-f", from, "--", recipients)
+	pipe, err := sendmail.StdinPipe()
 	if err != nil {
-		log.Println("Cannot connect to the API: " + value)
-	} else {
-		defer resp.Body.Close()
+		log.Println("Problem with sendmail() method: ", err)
 	}
+	err = sendmail.Start()
+	if err != nil {
+		log.Println("Problem with sendmail() method: ", err)
+	}
+
+	fmt.Fprintf(pipe, "Subject: %s\n\n", subject)
+	fmt.Fprintf(pipe, "%s", MailBody)
+	pipe.Close()
+	return err
+}
+
+func (call *APIStruct) api() error {
+	b := new(bytes.Buffer)
+	json.NewEncoder(b).Encode(call)
+	res, err := http.Post("http://localhost:8080/mails", "application/json; charset=utf-8", b)
+	if err != nil {
+		log.Println("Cannot connect to the API")
+		os.Exit(0)
+	}
+	defer res.Body.Close()
+	io.Copy(os.Stdout, res.Body)
+	return nil
 }
